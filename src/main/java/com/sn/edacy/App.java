1d61c410c90f6d1a0b938cc4f841f1b5933fcb68
tonux <sambndongo@gmail.com>
2018-05-19 11:34:09 +0000

    test

---
+++ src/main/java/com/sn/edacy/App.java
@@ -0,0 +1,13 @@
+package com.sn.edacy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Edacy!" );
+    }
+}
---
+++ src/test/java/com/sn/edacy/AppTest.java
@@ -0,0 +1,21 @@
+package com.sn.edacy;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertFalse( false );
+    }
+}
