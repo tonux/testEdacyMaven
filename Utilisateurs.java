aaed5e55666b1a232e291d3bb4613970037eb4c3
Ababacar KAMARA <24834704+ababacarkamara@users.noreply.github.com>
2018-08-16 18:43:17 +0000

    Add files via upload
    Ajout de la classe Utilisateurs

---
+++ Utilisateurs.java
@@ -0,0 +1,9 @@
+package com.sn.edacy;
+
+public class Utilisateurs {
+	protected String nom;
+	protected String prenom;
+	protected int telephone;
+	protected String mail;
+	protected String password;
+}
