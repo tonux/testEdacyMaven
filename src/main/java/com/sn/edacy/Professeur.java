3b20d5ebd1c904228b0cdc27f3fcdbc12432c772
tonux <sambndongo@gmail.com>
2018-08-16 19:03:39 +0000

    update class professeur

--- src/main/java/com/sn/edacy/Professeur.java
+++ src/main/java/com/sn/edacy/Professeur.java
@@ -5,4 +5,9 @@ public class Professeur {
 	private String prenom;
 	private String matiere;
 	private int NombreAnneExperience;
+
+
+	public void hello(){
+		System.out.println("Salam Magueye. ");
+	}
 }
