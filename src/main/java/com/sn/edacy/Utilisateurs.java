72af2550c23459ef9b507a5788963fcbbcb1bb1d
Ababacar KAMARA <ababacarkamara@gmail.com>
2018-08-17 01:05:12 +0000

    Add files via upload
    ajout class Utilisateurs

---
+++ src/main/java/com/sn/edacy/Utilisateurs.java
@@ -0,0 +1,42 @@
+package com.sn.edacy;
+
+public class Utilisateurs {
+	protected String nom;
+	protected String prenom;
+	protected int telephone;
+	protected String mail;
+	protected String password;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
